a7e840476a4ca640517a083d316e1436cd456192
CLIM\2358353 <2358353@cegeplimoilou.ca>
2025-05-02 09:53:49 -0400

    Création et implémentation  de  TP3FichierException

---
+++ src/main/java/h25/msd/poo2/etu/io/exception/TP3FichierException.java
@@ -0,0 +1,24 @@
+package h25.msd.poo2.etu.io.exception;
+
+import h25.msd.poo2.etu.exception.TP3Exception;
+import h25.msd.poo2.etu.utilisateur.AbstractUtilisateur;
+
+import java.io.File;
+
+public class TP3FichierException extends TP3Exception {
+
+
+    // doit conserver les fichiers impliqué trouvé une façon de faire
+
+    File file;
+
+    public TP3FichierException(String message, AbstractUtilisateur utilisateur , File file ) {
+        super(message, utilisateur);
+        this.file = file;
+
+    }
+
+    public File getFile() {
+        return file;
+    }
+}
