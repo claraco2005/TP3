270bc68b35f740f217b8f210eeca6d16055e6e78
diarra <2359013@cegeplimoilou.ca>
2025-05-02 09:26:35 -0400

    essai

--- src/main/java/h25/msd/poo2/etu/io/IO.java
+++ src/main/java/h25/msd/poo2/etu/io/IO.java
@@ -2,11 +2,13 @@ package h25.msd.poo2.etu.io;
 
 import h25.msd.poo2.echange.AlgorithmeI;
 
+import java.sql.SQLOutput;
+
 
 public class IO {
 
     public void sauvegardeAlgo(AlgorithmeI algo) {
-
+        System.out.println("efrcdgb v");
     }
 
     public AlgorithmeI chargeAlgo(String nomAlgoRecherche) {
